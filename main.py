2cb08f2164d9607f702598dbf0c1aad1514eaa87
remie92 <chris@hupse.eu>
2026-03-28 22:06:39 +0100

    Screenshotting the board!

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from pynput import mouse
+import pyautogui
+from PIL import Image
 from time import sleep
 
 
@@ -15,13 +17,31 @@ def on_click(x, y, button, pressed):
     if pressed:
         print(f"Clicked at: ({x}, {y})")
         clicks.append((x, y))
-        if len(clicks) == 2:
+        if len(clicks) == 3:
             return False  
 
 with mouse.Listener(on_click=on_click) as listener:
-    print("Click the top left of the dark background, and  then the bottom right. The border of the dark triangles count as the background.")
+    print("Click the top left of the dark background, and then the center dot of the golden marble. Lastly, the bottom right. The border of the dark triangles count as the background")
     listener.join()
 
 
-(screen_x1,screen_y1),(screen_x2,screen_y2)= clicks
+(screen_x1,screen_y1),(screen_middle_x,screen_middle_y),(screen_x2,screen_y2)= clicks
 print(screen_x1,screen_y1,screen_x2,screen_y2)
+
+left = min(screen_x1,screen_x2)
+top = min(screen_y1,screen_y2)
+right = max(screen_x1,screen_x2)
+bottom = max(screen_y1,screen_y2)
+
+width = right - left
+height = bottom - top
+
+print(f"Board is {width} pixels wide, and {height} pixels high!")
+
+
+screenshot = pyautogui.screenshot(region=(left, top, width, height))
+
+
+resized = screenshot.resize((833,688))
+
+resized.show()
