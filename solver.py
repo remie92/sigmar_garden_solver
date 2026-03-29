4828df5a1af270583816a23c6f3df529c889912f
remie92 <chris@hupse.eu>
2026-03-29 17:04:28 +0200

    added 2 helpers

--- solver.py
+++ solver.py
@@ -18,3 +18,55 @@ class Solver:
         if marble==None:
             return False
         
+
+    def get_row_length(self,row):
+        if row==0:
+            return 6
+        if row==1:
+            return 7
+        if row==2:
+            return 8
+        if row==3:
+            return 9
+        if row==4:
+            return 10
+        if row==5:
+            return 11
+        if row==6:
+            return 10
+        if row==7:
+            return 9
+        if row==8:
+            return 8
+        if row==9:
+            return 7
+        if row==10:
+            return 6
+        
+        
+
+    def index_to_row(self,index):
+        if index<6:
+            return 0
+        if index<13:
+            return 1
+        if index<21:
+            return 2
+        if index<30:
+            return 3
+        if index<40:
+            return 4
+        if index<51:
+            return 5
+        if index<61:
+            return 6
+        if index<70:
+            return 7
+        if index<78:
+            return 8
+        if index<85:
+            return 9
+        return 10
+        
+        
+        
